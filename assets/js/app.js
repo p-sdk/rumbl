8fd4078c9d3bb68d91efd1b5b77cff965814e1f7
p-sdk <paul.sdk@icloud.com>
2021-03-17 23:35:39 +0100

    Add a basic Video channel

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,12 +13,7 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+import socket from "./socket"
+import Video from "./video"
 
-import Player from "./player"
-let video = document.getElementById("video")
-
-if (video) {
-  Player.init(video.id, video.getAttribute("data-player-id"), () => {
-    console.log("player ready!")
-  })
-}
+Video.init(socket, document.getElementById("video"))
