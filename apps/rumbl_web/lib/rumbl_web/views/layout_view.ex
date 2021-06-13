eee4222ed5f71c79f795bb2f2ad10e350ad3d3f5
p-sdk <paul.sdk@icloud.com>
2021-06-13 21:20:54 +0200

    Add locale selection to the header

--- apps/rumbl_web/lib/rumbl_web/views/layout_view.ex
+++ apps/rumbl_web/lib/rumbl_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule RumblWeb.LayoutView do
   use RumblWeb, :view
+
+  def locale_selection(conn) do
+    for locale <- Gettext.known_locales(RumblWeb.Gettext) do
+      if locale == Gettext.get_locale(RumblWeb.Gettext) do
+        content_tag("strong", locale)
+      else
+        link(locale, to: conn.request_path <> "?locale=" <> locale)
+      end
+    end
+  end
 end
